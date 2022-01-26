60e6b97f6e43df5dcf5542a2fc9ac530a5f8bb56
ZahraO1 <zahra.masroor6@gmail.com>
2022-01-26 12:23:26 -0500

    added trivia questions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your Code Here
+let username = window.prompt("What art thy name?")
+let userScore = 0
+
+for (let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer == question.correctAnswer)
+        userScore += 10
+}
+window.alert("Your score is "+userScore)
